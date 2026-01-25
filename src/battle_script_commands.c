653a363dbb846e05337559bb25330dc93e56c823
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2026-01-25 15:32:32 +0000

    Fix checking uninitialized battle script ptr

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -4878,12 +4878,9 @@ static inline bool32 IsInstructionInBattleScriptsStack(const u8 *instr)
 {
     for (u32 i = gBattleResources->battleScriptsStack->size; i > 0; i++)
     {
-        if (gBattleResources->battleScriptsStack->ptr[i] == instr)
+        if (gBattleResources->battleScriptsStack->ptr[i - 1] == instr)
             return TRUE;
     }
-    // check if equal to first instruction which isn't checked above
-    if (gBattleResources->battleScriptsStack->ptr[0] == instr)
-        return TRUE;
     return FALSE;
 }
 
